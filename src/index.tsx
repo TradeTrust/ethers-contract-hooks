e191b572df4b20286b305da19b84b1c3d5aabce8
PH <68947167+osslgtm@users.noreply.github.com>
2024-09-26 17:06:00 +0800

    feat: upgrade packages

--- src/index.tsx
+++ src/index.tsx
@@ -66,7 +66,7 @@ export function useContractFunctionHook<T extends Contract, S extends keyof T["f
       const receipt = await transaction.wait();
       setState("CONFIRMED");
       setReceipt(receipt);
-    } catch (e) {
+    } catch (e: any) {
       setError(e);
       setState("ERROR");
     }
@@ -86,7 +86,7 @@ export function useContractFunctionHook<T extends Contract, S extends keyof T["f
       const response = await deferredTx;
       setState("CONFIRMED");
       setValue(response);
-    } catch (e) {
+    } catch (e: any) {
       setError(e);
       setState("ERROR");
     }
