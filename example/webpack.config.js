4cf111fe2ac117a1bc8f28dc04a29da5c9021d56
PH <68947167+osslgtm@users.noreply.github.com>
2024-12-09 17:12:38 +0800

    feat: upgrade packages (#2)

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -9,10 +9,11 @@ module.exports = {
     filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, "."),
     compress: true,
     port: 9001,
-    index: "index.html"
+    static: {
+      directory: path.join(__dirname, ".")
+    },
   },
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"]
